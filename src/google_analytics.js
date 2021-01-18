1811bdca708d631d4d8efb00a07fc63e775e9023
Glenn Stovall <glennstovall@gmail.com>
2021-01-18 10:39:15 -0500

    Fixes link and import issues

--- src/google_analytics.js
+++ src/google_analytics.js
@@ -1,5 +1,5 @@
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export function trackPageview(url, trackingId) {
+const trackPageview = (url, trackingId) => {
   if (!window.gtag) return
   window.gtag("config", trackingId, {
     page_path: url,
@@ -7,9 +7,12 @@ export function trackPageview(url, trackingId) {
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export function trackEvent(action, event_category, event_label, value) {
+const trackEvent = (action, event_category, event_label, value) => {
   if (!window.gtag) return
   window.gtag("event", action, {
     event_category, event_label, value
   })
 }
+
+export default { trackPageview, trackEvent }
+
